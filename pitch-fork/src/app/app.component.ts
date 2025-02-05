d66bad0ad034b83bd74bd2dab3d0b73bd19dc343
lukeromburgh <i87v8t9s@students.codeinstitute.net>
2025-02-05 17:56:53 +0000

    Post Card
    attempting to make dynamic ang components for post-cards and the post page

--- pitch-fork/src/app/app.component.ts
+++ pitch-fork/src/app/app.component.ts
@@ -3,10 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { PostCardComponent } from './components/post-card/post-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    PostCardComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ pitch-fork/src/app/components/post-card/post-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-post-card',
+  templateUrl: './post-card.component.html',
+  styleUrls: ['./post-card.component.scss'],
+})
+export class PostCardComponent {
+  @Input() post: any; // Receives post data dynamically
+
+  constructor(private router: Router) {}
+
+  viewPost() {
+    this.router.navigate(['/post', this.post.id]); // Navigate to post page
+  }
+}
