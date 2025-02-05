b917271febdea99e1bd60dc4d53204b4f0854a45
lukeromburgh <i87v8t9s@students.codeinstitute.net>
2025-02-05 17:42:29 +0000

    Footer sticky to bottom of page

--- pitch-fork/src/app/app.component.ts
+++ pitch-fork/src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, NavbarComponent],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'pitch-fork';
 }
-
