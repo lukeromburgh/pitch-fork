d928e55b12b650c82a250eab77bc27f3af17599b
lukeromburgh <i87v8t9s@students.codeinstitute.net>
2025-02-05 11:48:25 +0000

    HomeComponent
    Setting up home component, and designing landing page

--- pitch-fork/src/app/app.component.ts
+++ pitch-fork/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomeComponent],
+  templateUrl: './home/home.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
