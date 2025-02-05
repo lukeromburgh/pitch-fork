759f72cbc81ac5036c69ed431efce48ec6b189f6
lukeromburgh <i87v8t9s@students.codeinstitute.net>
2025-02-05 12:03:47 +0000

    Fix app/home logic

--- pitch-fork/src/app/app.component.ts
+++ pitch-fork/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HomeComponent],
-  templateUrl: './home/home.component.html',
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
