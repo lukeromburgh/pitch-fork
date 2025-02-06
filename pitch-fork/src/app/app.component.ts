a1df2b037898b142fbc0675eef7dec60884b69b6
lukeromburgh <i87v8t9s@students.codeinstitute.net>
2025-02-06 20:27:31 +0000

    Fix location of post cards

--- pitch-fork/src/app/app.component.ts
+++ pitch-fork/src/app/app.component.ts
@@ -8,17 +8,10 @@ import {
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { PostCardComponent } from './components/post-card/post-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    HomeComponent,
-    NavbarComponent,
-    FooterComponent,
-    PostCardComponent,
-  ],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
--- pitch-fork/src/app/components/home/home.component.ts
+++ pitch-fork/src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { PostCardComponent } from '../post-card/post-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, PostCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   standalone: true,
