6257fae50ec6b730653975ab88aa30397689fb6a
lukeromburgh <i87v8t9s@students.codeinstitute.net>
2025-03-25 19:24:34 +0000

    change baseUrl

--- pitch-fork/src/environments/environment.prod.ts
+++ pitch-fork/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://pitch-fork-1.onrender.com/api', // When deployed, API calls will be relative to the domain
-  baseUrl: 'https://pitch-fork-1.onrender.com/',
+  baseUrl: 'https://pitch-fork-1.onrender.com',
   // If you're deploying to a specific domain, you might set this to:
   // apiUrl: 'https://your-production-domain.com/api'
 };
