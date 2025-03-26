d43b68f9811ce51de17ac037cdf850f32fb66cb7
lukeromburgh <i87v8t9s@students.codeinstitute.net>
2025-03-26 15:16:34 +0000

    Mobile css optimization, responsive design, mobile first design, etc.

--- pitch-fork/src/app/components/navbar/navbar.component.ts
+++ pitch-fork/src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
   standalone: true,
 })
 export class NavbarComponent {
+  menuOpen = false;
+
   constructor(public router: Router) {}
 
   Home() {
     this.router.navigate(['/']);
+    this.menuOpen = false; // Close menu on navigation
+  }
+
+  logoOnly() {
+    return window.innerWidth < 768; // Hide text on mobile
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
   }
 }
